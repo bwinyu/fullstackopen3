686506bc1841e7f1fd666a0a4c804b99af15138c
Baldwin Yu <baldwiny@live.ca>
2022-10-03 23:35:01 -0600

    3a

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { application } = require('express');
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+
+morgan.token('postdata', (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'));
 
 let persons = [
   { 
